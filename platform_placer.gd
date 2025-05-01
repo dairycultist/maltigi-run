848af6f2a9970047a9cf93b2543a0ac7daef266f
BlueBunnex <75032462+BlueBunnex@users.noreply.github.com>
2025-05-01 13:30:40 -0400

    Circular maps

--- platform_placer.gd
+++ platform_placer.gd
@@ -1,16 +1,20 @@
 extends Node3D
 
 const PLATFORM = preload("res://platform.tscn")
+const DIAMETER_TO_HEIGHT = sqrt(3) / 2.0
 
 @export var hex_diameter := 17.32
-const DIAMETER_TO_HEIGHT = sqrt(3) / 2.0
+@export var map_size := 4
 
 func _ready() -> void:
 	
 	var random := RandomNumberGenerator.new()
 	
-	for x in range(-2, 3):
-		for z in range(-2, 3):
+	for x in range(1 - map_size, map_size):
+		for z in range(1 - map_size, map_size):
+			
+			if abs(x + z) >= map_size and abs(x - z) <= map_size / 2:
+				continue
 		
 			var platform = PLATFORM.instantiate()
 			add_child(platform)
