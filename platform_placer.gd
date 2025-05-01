3510c6a2594f18d02cc242b1f292587a0ca79116
BlueBunnex <75032462+BlueBunnex@users.noreply.github.com>
2025-05-01 13:12:11 -0400

    Tiling

--- platform_placer.gd
+++ platform_placer.gd
@@ -1,3 +1,15 @@
 extends Node3D
 
-@export var hex_radius := 17.32
+const PLATFORM = preload("res://platform.tscn")
+
+@export var hex_diameter := 17.32
+const DIAMETER_TO_HEIGHT = sqrt(3) / 2.0
+
+func _ready() -> void:
+	
+	for x in range(-2, 3):
+		for z in range(-2, 3):
+		
+			var platform = PLATFORM.instantiate()
+			add_child(platform)
+			platform.position = Vector3((x + z * 0.5) * hex_diameter, 0, z * hex_diameter * DIAMETER_TO_HEIGHT)
