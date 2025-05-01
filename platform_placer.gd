953b3a31c03a77e23c098eb106079be795604bdf
BlueBunnex <75032462+BlueBunnex@users.noreply.github.com>
2025-05-01 13:35:54 -0400

    Clean up code; Add water floor

--- platform_placer.gd
+++ platform_placer.gd
@@ -4,7 +4,7 @@ const PLATFORM = preload("res://platform.tscn")
 const DIAMETER_TO_HEIGHT = sqrt(3) / 2.0
 
 @export var hex_diameter := 17.32
-@export var map_size := 7
+@export var map_size := 3
 
 func _ready() -> void:
 	
@@ -13,14 +13,17 @@ func _ready() -> void:
 	for x in range(1 - map_size, map_size):
 		for z in range(1 - map_size, map_size):
 			
+			# circularize
 			if abs(x + z) >= map_size and abs(x - z) <= map_size - 2:
 				continue
 		
 			var platform = PLATFORM.instantiate()
 			add_child(platform)
+			
+			# place platform
 			platform.position = Vector3(
 				(x + z * 0.5) * hex_diameter,
-				0, #random.randf_range(3.0, 20.0),
+				random.randi_range(1, 10) * 2,
 				z * hex_diameter * DIAMETER_TO_HEIGHT
 			)
 			platform.rotation = Vector3(0, PI / 3.0 * random.randi_range(0, 6), 0)
